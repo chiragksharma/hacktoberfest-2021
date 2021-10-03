86f10b399a15bbc6d6a27027e0887f3015dd7cee
chiragksharma <chiragksharma15@gmail.com>
2021-10-03 09:51:06 +0530

    Added Day name in this Digital Clock.
    Adding Day name also in index.js. 
    It will display the present-day name.

--- JavaScript/Digital-Time-Date/index.js
+++ JavaScript/Digital-Time-Date/index.js
@@ -3,5 +3,12 @@ function setClock() {
     let date = new Date().toLocaleDateString('en-GB');
     document.querySelector('.clock').innerHTML = `<h1>${time}</h1><p>${date}</p>` ;
 }
+var d_names = ["Sunday","Monday", "Tuesday", "Wednesday", 
+"Thursday", "Friday", "Saturday"];
+var myDate = new Date();
+myDate.setDate(myDate.getDate()+7);
+var curr_day  = myDate.getDay();
+document.write(`<h1 style="color: red;text-align: center;">${d_names[curr_day]}</h1>`);
+
   
 setInterval(setClock, 1000);
